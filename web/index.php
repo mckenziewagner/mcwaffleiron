6624e94899bca494faa97a886b6ec721015c4260
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-06-21 15:10:22 -0500

    dope

---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+// Tells WordPress to load the WordPress theme and output it.
+
+define('WP_USE_THEMES', true);
+// Loads the WordPress Environment and Template
+//$wp_init_path = realpath( __DIR__ . '/../vendor/pantheon-systems/wordpress/wp-blog-header.php' );
+//echo $wp_init_path;
+require('cms/wp-blog-header.php');
